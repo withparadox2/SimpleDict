b3fb4be8e25a97a9f524539bd9a6903e9f336570
withparadox2 <withparadox2@gmail.com>
2017-08-13 20:56:38 +0800

    Add todos.

---
+++ app/src/main/java/com/withparadox2/simpledict/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.withparadox2.simpledict;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by withparadox2 on 2017/8/12.
+ */
+
+public class HomeActivity extends Activity {
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
