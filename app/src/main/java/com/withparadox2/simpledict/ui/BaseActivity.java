e1ea915874c7de402612ae8741d99ae47ed159c7
withparadox2 <withparadox2@gmail.com>
2017-10-02 19:07:35 +0800

    Action bar title.

--- app/src/main/java/com/withparadox2/simpledict/ui/BaseActivity.java
+++ app/src/main/java/com/withparadox2/simpledict/ui/BaseActivity.java
@@ -33,8 +33,8 @@ public class BaseActivity extends AppCompatActivity {
   }
 
   public void setTitle(String title) {
-    if (getActionBar() != null) {
-      getActionBar().setTitle(title);
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setTitle(title);
     }
   }
 }
